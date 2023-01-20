5b45e9e05655e3a2d1b3700bcaaf065cb0008146
Tom Aldcroft <taldcroft@gmail.com>
2023-01-20 17:34:27 -0500

    De-namespace

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from setuptools import setup
-setup(name='Ska.astro',
+from ska_helpers.setup_helper import duplicate_package_info
+
+name = "ska_astro"
+namespace = "Ska.astro"
+
+packages = ["ska_astro"]
+package_dir = {name: name}
+
+duplicate_package_info(packages, name, namespace)
+duplicate_package_info(package_dir, name, namespace)
+
+setup(name=name,
       author = 'Tom Aldcroft',
       description='Astronomy utilities',
-      author_email = 'aldcroft@head.cfa.harvard.edu',
-      py_modules = ['Ska.astro'],
+      author_email = 'taldcroft@cfa.harvard.edu',
       use_scm_version=True,
       setup_requires=['setuptools_scm', 'setuptools_scm_git_archive'],
       zip_safe=False,
-      packages=['Ska'],
-      package_dir={'Ska' : 'Ska'},
-      package_data={}
+      packages=packages,
+      package_dir=package_dir,
       )
